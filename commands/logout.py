e3088d453473094a4ff5f068fb63ce8329d5d327
unillr <55926286+unillr@users.noreply.github.com>
2022-10-14 23:18:13 +0900

    logoutの実装

---
+++ commands/logout.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+
+@commands.hybrid_command()
+@commands.is_owner()
+async def logout(ctx):
+    '''Botのログアウト(開発用)'''
+    await ctx.send('Goodbye!')
+    await ctx.bot.close()
+
+async def setup(bot):
+    bot.add_command(logout)
