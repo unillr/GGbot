60524fa8a9caf9365feb001febf293d0c6644503
unillr <55926286+unillr@users.noreply.github.com>
2022-10-15 00:37:27 +0900

    ephemeralに変更

--- commands/logout.py
+++ commands/logout.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 @commands.is_owner()
 async def logout(ctx):
     '''Botのログアウト(開発用)'''
-    await ctx.send('Goodbye!')
+    await ctx.send('Goodbye!', ephemeral=True)
     await ctx.bot.close()
 
 async def setup(bot):
