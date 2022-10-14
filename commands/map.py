7fb78287ce8d2df61463bccfb968e83f24771dca
unillr <55926286+unillr@users.noreply.github.com>
2022-10-14 18:21:05 +0900

    /map BAN機能の追加

--- commands/map.py
+++ commands/map.py
@@ -5,30 +5,24 @@ import discord
 from discord.ext import commands
 
 
-MAPIMAGES_DIR = 'images/maps'
+class MapSelectMenu(discord.ui.View):
+    maps = [m[:-4] for m in os.listdir(f'./images/maps/VALORANT') if m.endswith('.png')]
+    options = [discord.SelectOption(label=l) for l in maps]
 
-
-class GameSelectButton(discord.ui.View):
-    @discord.ui.button(label='VALORANT', style=discord.ButtonStyle.red)
-    async def map_valorant(self, interaction: discord.Integration, button: discord.ui.Button):
-        mapimages = list(filter(lambda file: file.endswith('.png'), os.listdir(f'./{MAPIMAGES_DIR}/VALORANT')))
-        mapimage = random.choice(mapimages)
-        embed = discord.Embed(title=mapimage.removesuffix('.png').upper(), colour=discord.Colour.red())
-        embed.set_image(url=f'attachment://{mapimage}')
-        attachment = discord.File(f'./{MAPIMAGES_DIR}/VALORANT/{mapimage}', filename=mapimage)
-        await interaction.response.edit_message(embed=embed, attachments=[attachment])
-
-    @discord.ui.button(label='Splatoon', style=discord.ButtonStyle.grey, disabled=True)
-    async def map_splatoon(self, interaction: discord.Integration, button: discord.ui.Button):
-        pass
+    @discord.ui.select(placeholder='BAN MAPを選択…', min_values=0, max_values=len(options)-1, options=options)
+    async def ban_map(self, interaction: discord.Integration, select: discord.ui.Select):
+        pickedmap = random.choice([m for m in self.maps if m not in select.values])
+        embed = discord.Embed(title=pickedmap, colour=discord.Colour.red())
+        embed.set_image(url=f'attachment://{pickedmap}.png')
+        attachment = discord.File(f'./images/maps/VALORANT/{pickedmap}.png', filename=f'{pickedmap}.png')
+        await interaction.response.edit_message(embed=embed, attachments=[attachment], view=None)
 
 
 @commands.hybrid_command()
 async def map(ctx):
-    '''ランダムにマップを返す'''
-    embed = discord.Embed(title='ゲームを選択', colour=discord.Colour.blurple())
-    await ctx.send(embed=embed, view=GameSelectButton())
+    '''VALORANTのマップをランダムに選択'''
+    await ctx.send(view=MapSelectMenu())
+
 
 async def setup(bot):
     bot.add_command(map)
-
