607c7c01d8a45b104443f9c0b2c683d3ff1eeda0
unillr <55926286+unillr@users.noreply.github.com>
2022-10-15 20:47:49 +0900

    候補が表示されるように

--- commands/reload.py
+++ commands/reload.py
@@ -1,14 +1,20 @@
+import os
+
 from discord import app_commands
 from discord.ext import commands
 
 
-@commands.hybrid_command()
+extensions = [e[:-3] for e in os.listdir(f'./commands') if e.endswith('.py')]
+
+
+@commands.hybrid_command(name='reload')
 @commands.is_owner()
-@app_commands.describe(command='リロードするコマンド')
-async def reload(ctx, command: str):
-    '''コマンドをリロード(開発用)'''
-    await ctx.bot.reload_extension('commands.' + command)
+@app_commands.describe(extension='リロードするエクステンション')
+@app_commands.choices(extension=[app_commands.Choice(name=e, value=e) for e in extensions])
+async def reload_extension(ctx, extension: app_commands.Choice[str]):
+    '''エクステンションをリロード(開発用)'''
+    await ctx.bot.reload_extension('commands.' + extension.name)
     await ctx.send('Successfully reloaded.', ephemeral=True)
 
 async def setup(bot):
-    bot.add_command(reload)
+    bot.add_command(reload_extension)
