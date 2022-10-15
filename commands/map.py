71db636843244f94439d63ee4f794ee554b3fea9
unillr <55926286+unillr@users.noreply.github.com>
2022-10-15 16:37:01 +0900

    他のゲームに対応できるよう変更

--- commands/map.py
+++ commands/map.py
@@ -5,24 +5,32 @@ import discord
 from discord.ext import commands
 
 
-class MapSelectMenu(discord.ui.View):
-    maps = [m[:-4] for m in os.listdir(f'./images/maps/VALORANT') if m.endswith('.png')]
-    options = [discord.SelectOption(label=l) for l in maps]
+class MapSelectMenu(discord.ui.Select):
+    def __init__(self, game):
+        self.game = game
+        self.maps = [m[:-4] for m in os.listdir(f'./images/maps/{self.game}') if m.endswith('.png')]
+        options = [discord.SelectOption(label=m) for m in self.maps]
+        super().__init__(placeholder='除外するマップを選択…', min_values=0, max_values=len(options) - 1, options=options)
 
-    @discord.ui.select(placeholder='BAN MAPを選択…', min_values=0, max_values=len(options)-1, options=options)
-    async def ban_map(self, interaction: discord.Integration, select: discord.ui.Select):
-        pickedmap = random.choice([m for m in self.maps if m not in select.values])
+    async def callback(self, interaction: discord.Integration):
+        pickedmap = random.choice([m for m in self.maps if m not in self.values])
         embed = discord.Embed(title=pickedmap, colour=discord.Colour.red())
         embed.set_image(url=f'attachment://{pickedmap}.png')
-        embed.set_footer(text='BAN: ' + (' '.join(select.values) or 'なし'))
-        attachment = discord.File(f'./images/maps/VALORANT/{pickedmap}.png', filename=f'{pickedmap}.png')
-        await interaction.response.edit_message(embed=embed, attachments=[attachment], view=None)
+        embed.set_footer(text='BAN: ' + (' '.join(self.values) or 'なし'))
+        attachments = [discord.File(f'./images/maps/{self.game}/{pickedmap}.png', filename=f'{pickedmap}.png')]
+        await interaction.response.edit_message(embed=embed, attachments=attachments, view=None)
+
+
+class MapSelectMenuView(discord.ui.View):
+    def __init__(self, game):
+        super().__init__()
+        self.add_item(MapSelectMenu(game))
 
 
 @commands.hybrid_command()
 async def map(ctx):
     '''VALORANTのマップをランダムに選択'''
-    await ctx.send(view=MapSelectMenu())
+    await ctx.send(view=MapSelectMenuView('VALORANT'))
 
 
 async def setup(bot):
